85dc8b3ce965c0fe81e902ecc161e54c4cf11b5b
Adeshina Adenuga <nuga0909@gmail.com>
2022-11-14 23:01:32 +0100

    update css file to show nav items

--- home.js
+++ home.js
@@ -1,12 +1,14 @@
 /* eslint-disable linebreak-style */
-const hamburger = document.querySelector('#hamburger1');
-const nav = document.querySelector('#nav-desktop');
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active');
-  nav.classList.toggle('active');
-});
+window.onload=function() {
+  const hamburger = document.querySelector('#nav-mobile');
+  const nav = document.querySelector('.nav-lists');
+  hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    nav.classList.toggle('active');
+  });
 
-document.querySelectorAll('#nav-mobile').forEach((n) => n.addEventListener('click', () => {
-  hamburger.classList.remove('active');
-  nav.classList.remove('active');
-}));
+  document.querySelectorAll('.nav-list').forEach((n) => n.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    nav.classList.remove('active');
+  }));
+};
