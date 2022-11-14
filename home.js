839a28ed36ca1e874743f1e139530714b56c747a
Adeshina Adenuga <nuga0909@gmail.com>
2022-11-14 14:44:43 +0100

    add eventlistener to hamburger

--- home.js
+++ home.js
@@ -1,2 +1,12 @@
-const nav = document.querySelector('#nav-mobile');
-console.log(12333);
+/* eslint-disable linebreak-style */
+const hamburger = document.querySelector('#hamburger');
+const nav = document.querySelector('#nav-desktop');
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  nav.classList.toggle('active');
+});
+
+document.querySelectorAll('#nav-desktop').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  nav.classList.remove('active');
+}));
