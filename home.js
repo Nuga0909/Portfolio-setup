270739320a509cb3687a28f5728cc38862d994e8
bernabasy <104406349+bernabasy@users.noreply.github.com>
2022-11-14 17:33:00 +0300

    change the id for the query selector

--- home.js
+++ home.js
@@ -1,12 +1,12 @@
 /* eslint-disable linebreak-style */
-const hamburger = document.querySelector('#hamburger');
+const hamburger = document.querySelector('#hamburger1');
 const nav = document.querySelector('#nav-desktop');
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   nav.classList.toggle('active');
 });
 
-document.querySelectorAll('#nav-desktop').forEach((n) => n.addEventListener('click', () => {
+document.querySelectorAll('#nav-mobile').forEach((n) => n.addEventListener('click', () => {
   hamburger.classList.remove('active');
   nav.classList.remove('active');
 }));
